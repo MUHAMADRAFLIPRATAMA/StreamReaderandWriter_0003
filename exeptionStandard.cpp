11ed664553aabd8d9b98db84eda776207474d1a1
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-06-09 13:29:43 +0700

    /*akan di eksekusi karna array data hanya memiliki 3 elemen*/

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -16,4 +16,7 @@ int main()
     }
     catch (exception& e) {
         //penangkap menggunakan obyek exception
-       
+        cout << e.what() << endl;
+        /*akan di eksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    
