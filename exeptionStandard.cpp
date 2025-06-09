1ff86b4383d61b5792940dfec04110f80cab1218
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-06-09 13:29:55 +0700

    *penanda 2: bahwa pogram berjalan tanpa berhenti meskipun terjadi kesalahan*/

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -19,4 +19,8 @@ int main()
         cout << e.what() << endl;
         /*akan di eksekusi karna array data hanya memiliki 3 elemen*/
     }
+    cout << "Baris Program yang terakhir" << endl;
+    /*penanda 2: bahwa pogram berjalan tanpa berhenti meskipun terjadi kesalahan*/
     
+    return 0;
+}
