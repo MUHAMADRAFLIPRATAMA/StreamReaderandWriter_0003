9609ad5d338d308c809a5ac63b05db77944ca4d3
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-06-09 13:15:17 +0700

    jika file ada maka

--- ioFile.cpp
+++ ioFile.cpp
@@ -32,4 +32,7 @@ int main(){
     infile.open("contohfile.txt");
 
     cout << endl << ">= Membuka dan membaca file" << endl;
-    
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        
