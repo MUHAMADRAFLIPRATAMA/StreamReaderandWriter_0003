b1f38edba81e15b0c6a0291e5797c4c65a83e852
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-06-09 13:16:52 +0700

    Program meminta pengguna untuk memasukkan nama file,

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string namaFile;
+
+    cout << "masukkan nama file: ";
+    cin >> namaFile;
+
+    
